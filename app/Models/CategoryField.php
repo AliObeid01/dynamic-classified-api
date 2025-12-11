d0a969f100249efb2da9488c7b956dd8eacb8f7f
Ali Obeid <a.obeid@atmit.net>
2025-12-11 15:18:27 +0200

    create category field model

---
+++ app/Models/CategoryField.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryField extends Model
+{
+    protected $fillable = [
+        'category_id','name','attribute','value_type','filter_type','is_mandatory'
+    ];
+
+    protected $casts = [
+        'is_mandatory' => 'bool',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class,'category_id');
+    }
+}
