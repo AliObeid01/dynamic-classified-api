2f1d007fcef6d42bd79bd373c59def3cd6789ffe
Ali Obeid <a.obeid@atmit.net>
2025-12-11 15:21:09 +0200

    create categoru field option model

---
+++ app/Models/CategoryFieldOption.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryFieldOption extends Model
+{
+    protected $fillable = [
+        'category_field_id','parent_option_id','slug','value','label','label_l1'
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_option_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_option_id');
+    }
+
+    public function categoryField()
+    {
+        return $this->belongsTo(CategoryField::class,'category_field_id');
+    }
+}
